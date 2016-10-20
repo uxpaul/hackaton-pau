ad53e31055c42a2836f772ac831c65eeb1a69628
uxpaul <hoedevproject@gmail.com>
2016-10-20 07:44:31 +0200

    Add profil

---
+++ src/js/components/profil/profil.js
@@ -0,0 +1,12 @@
+((app)=> {
+  'use strict'
+  app.component('profil', {
+    templateUrl:'js/components/profil/profil.html',
+    controller: function(){
+      angular.extend(this, {
+
+
+      })
+    }
+  })
+})(angular.module('app.profil'))

e63180599827b8a10d631a36429eab9ec27b371b
apprenti <nans.mourgues@hotmail.fr>
2016-10-20 07:55:05 +0200

    I'm ready

--- src/js/components/quiz/quiz.md.js
+++
@@ -1,9 +0,0 @@
-((app) => {
-    'use strict'
-    app.config(['$stateProvider', ($stateProvider) => {
-        $stateProvider.state('app.quiz', {
-            url: '/quiz',
-            template: '<quiz />'
-        })
-    }])
-})(angular.module('app.quiz', []))
